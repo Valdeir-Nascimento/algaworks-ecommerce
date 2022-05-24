6eb8785e4839f47414f8081c83fc67ec9b82c106
Valdeir Nascimento <valdeircn11@gmail.com>
2022-05-23 21:22:21 -0300

    Mapeando mapas com @ElementCollection

--- src/test/java/com/algaworks/ecommerce/mapeamentoavancao/ElementCollectionTest.java
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancao/ElementCollectionTest.java
@@ -2,11 +2,15 @@ package com.algaworks.ecommerce.mapeamentoavancao;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Atributo;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Produto;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class ElementCollectionTest extends EntityManagerTest {
@@ -36,6 +40,20 @@ public class ElementCollectionTest extends EntityManagerTest {
 
 		Produto produtoVerificado = entityManager.find(Produto.class, produto.getId());
 		assertFalse(produtoVerificado.getAtributos().isEmpty());
+
+	}
+
+	@Test
+	public void aplicarContato() {
+		entityManager.getTransaction().begin();
+		Cliente cliente = entityManager.find(Cliente.class, 1);
+		cliente.setContatos(Collections.singletonMap("email", "fernando@email.com"));
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Cliente clienteVerificado = entityManager.find(Cliente.class, cliente.getId());
+		assertEquals("fernando@email.com", clienteVerificado.getContatos().get("email") );
 
 	}
 
