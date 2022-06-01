b7c66a549817c3743adf3d5012f7d00bbb303bde
Valdeir Nascimento <valdeircn11@gmail.com>
2022-06-01 20:41:21 -0300

    Mapeando herança com estratégia de uma tabela por classe (table per class)

--- src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
+++ src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
@@ -7,9 +7,9 @@ import javax.persistence.*;
 
 @Getter
 @Setter
-@DiscriminatorValue("boleto")
+//@DiscriminatorValue("boleto")
 @Entity
-//@Table(name = "pagamento_boleto")
+@Table(name = "pagamento_boleto")
 public class PagamentoBoleto extends Pagamento {
 
 	@Column(name = "codigo_barras")
