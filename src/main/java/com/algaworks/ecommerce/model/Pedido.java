168ee333b263ff2a1982145725362c86691a9b55
Valdeir Nascimento <valdeircn11@gmail.com>
2022-11-12 21:00:55 -0300

    Fazendo atualizações em cascata

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -12,90 +12,92 @@ import java.util.List;
 
 @Getter
 @Setter
-@EntityListeners({ GerarNotaFiscalListener.class, GenericoListener.class })
+@EntityListeners({GerarNotaFiscalListener.class, GenericoListener.class})
 @Entity
 @Table(name = "pedido")
 public class Pedido extends EntidadeBaseInteger {
 
-	@ManyToOne(optional = false)
-	@JoinColumn(name = "cliente_id", nullable = false, foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
-	private Cliente cliente;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "cliente_id", nullable = false, foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
+    private Cliente cliente;
 
-	@OneToMany(mappedBy = "pedido")
-	private List<ItemPedido> itens;
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.MERGE)
+    private List<ItemPedido> itens;
 
-	@Column(name = "data_criacao", updatable = false, nullable = false)
-	private LocalDateTime dataCriacao;
+    @Column(name = "data_criacao", updatable = false, nullable = false)
+    private LocalDateTime dataCriacao;
 
-	@Column(name = "data_ultima_atualizacao", insertable = false)
-	private LocalDateTime dataUltimaAtualizacao;
+    @Column(name = "data_ultima_atualizacao", insertable = false)
+    private LocalDateTime dataUltimaAtualizacao;
 
-	@Column(name = "data_conclusao")
-	private LocalDateTime dataConclusao;
+    @Column(name = "data_conclusao")
+    private LocalDateTime dataConclusao;
 
-	@OneToOne(mappedBy = "pedido")
-	private NotaFiscal notaFiscal;
+    @OneToOne(mappedBy = "pedido")
+    private NotaFiscal notaFiscal;
 
-	@Column(nullable = false)
-	private BigDecimal total;
+    @Column(nullable = false)
+    private BigDecimal total;
 
-	@Column(length = 30, nullable = false)
-	@Enumerated(EnumType.STRING)
-	private StatusPedido status;
+    @Column(length = 30, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
 
-	@OneToOne(mappedBy = "pedido")
-	private Pagamento pagamento;
+    @OneToOne(mappedBy = "pedido")
+    private Pagamento pagamento;
 
-	@Embedded
-	private EnderecoEntregaPedido enderecoEntrega;
+    @Embedded
+    private EnderecoEntregaPedido enderecoEntrega;
 
-	public boolean isPago() {
-		return StatusPedido.PAGO.equals(status);
-	}
+    public boolean isPago() {
+        return StatusPedido.PAGO.equals(status);
+    }
 
-	//    @PrePersist
+    //    @PrePersist
 //    @PreUpdate
-	public void calcularTotal() {
-		if (itens != null) {
-			total = itens.stream().map(ItemPedido::getPrecoProduto)
-					.reduce(BigDecimal.ZERO, BigDecimal::add);
-		}
-	}
+    public void calcularTotal() {
+        if (itens != null) {
+            total = itens.stream().map(item -> new BigDecimal(item.getQuantidade()).multiply(item.getPrecoProduto()))
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+        } else {
+            total = BigDecimal.ZERO;
+        }
+    }
 
-	@PrePersist
-	public void aoPersistir() {
-		dataCriacao = LocalDateTime.now();
-		calcularTotal();
-	}
+    @PrePersist
+    public void aoPersistir() {
+        dataCriacao = LocalDateTime.now();
+        calcularTotal();
+    }
 
-	@PreUpdate
-	public void aoAtualizar() {
-		dataUltimaAtualizacao = LocalDateTime.now();
-		calcularTotal();
-	}
+    @PreUpdate
+    public void aoAtualizar() {
+        dataUltimaAtualizacao = LocalDateTime.now();
+        calcularTotal();
+    }
 
-	@PostPersist
-	public void aposPersistir() {
-		System.out.println("Após persistir Pedido.");
-	}
+    @PostPersist
+    public void aposPersistir() {
+        System.out.println("Após persistir Pedido.");
+    }
 
-	@PostUpdate
-	public void aposAtualizar() {
-		System.out.println("Após atualizar Pedido.");
-	}
+    @PostUpdate
+    public void aposAtualizar() {
+        System.out.println("Após atualizar Pedido.");
+    }
 
-	@PreRemove
-	public void aoRemover() {
-		System.out.println("Antes de remover Pedido.");
-	}
+    @PreRemove
+    public void aoRemover() {
+        System.out.println("Antes de remover Pedido.");
+    }
 
-	@PostRemove
-	public void aposRemover() {
-		System.out.println("Após remover Pedido.");
-	}
+    @PostRemove
+    public void aposRemover() {
+        System.out.println("Após remover Pedido.");
+    }
 
-	@PostLoad
-	public void aoCarregar() {
-		System.out.println("Após carregar o Pedido.");
-	}
+    @PostLoad
+    public void aoCarregar() {
+        System.out.println("Após carregar o Pedido.");
+    }
 }
