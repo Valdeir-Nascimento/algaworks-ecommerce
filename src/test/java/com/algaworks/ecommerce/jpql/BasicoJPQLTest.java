d00c0bd30d6cef960f4ed9ad10d5b2b97b594be4
Valdeir Nascimento <valdeircn11@gmail.com>
2022-11-16 15:35:45 -0300

    Introdução ao JPQL

--- src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/BasicoJPQLTest.java
@@ -1,13 +1,17 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.dto.ProdutoDTO;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 import org.junit.Test;
 
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class BasicoJPQLTest extends EntityManagerTest {
 
@@ -28,7 +32,37 @@ public class BasicoJPQLTest extends EntityManagerTest {
         Query query = entityManager.createQuery(jpql);
         Pedido pedidoAtual = (Pedido) query.getSingleResult();
         assertNotNull(pedidoAtual);
+    }
 
+    @Test
+    public void selecionarUmAtributoParaRetorno() {
+        String jpql = "select p.nome from Produto p";
+        TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+        List<String> produtos = typedQuery.getResultList();
+        assertTrue(String.class.equals(produtos.get(0).getClass()));
+
+        String jpqlCliente = "select p.cliente from Pedido p";
+        TypedQuery<Cliente> typedQueryCliente = entityManager.createQuery(jpqlCliente, Cliente.class);
+        List<Cliente> clientes = typedQueryCliente.getResultList();
+        assertTrue(Cliente.class.equals(clientes.get(0).getClass()));
+    }
+
+    @Test
+    public void projetarResultado() {
+        String jpql = "select id, nome from Produto";
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> resposta = typedQuery.getResultList();
+        assertEquals(2, resposta.get(0).length);
+        resposta.forEach(item -> System.out.println(item[0] + ", " + item[1]));
+    }
+
+    @Test
+    public void projetarDTO() {
+        String jpql = "select new com.algaworks.ecommerce.dto.ProdutoDTO(id, nome) from Produto";
+        TypedQuery<ProdutoDTO> typedQuery = entityManager.createQuery(jpql, ProdutoDTO.class);
+        List<ProdutoDTO> produtos = typedQuery.getResultList();
+        assertFalse(produtos.isEmpty());
+        produtos.forEach(produto -> System.out.println(produto.getNome()));
     }
 
 }
