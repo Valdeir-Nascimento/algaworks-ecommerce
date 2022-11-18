c08adb780ad1a87318f8c1ee23e9d17544b96aa5
Valdeir Nascimento <valdeircn11@gmail.com>
2022-11-18 15:28:41 -0300

    Agrupando o registros com group by

--- src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -18,14 +18,25 @@ public class GroupByTest extends EntityManagerTest {
 
         // Total de vendas por categoria: select c.nome, sum(ip.precoProduto) from ItemPedido ip join ip.produto pro join pro.categorias c group by c.id
 
-        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+        // Total de vendas por cliente: select c.nome, sum(p.total) from ItemPedido ip join ip.pedido p join p.cliente c group by c.id
+
+        // Total de vendas por dia e por categoria:
+
+        String jpql = "select " +
+                "concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), " +
+                "concat(c.nome, ': ', sum(ip.precoProduto)) " +
+                "from ItemPedido ip " +
+                "join ip.pedido p " +
                 "join ip.produto pro " +
                 "join pro.categorias c " +
-                "group by c.id";
+                "group by year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao), c.id " +
+                "order by p.dataCriacao, c.nome";
+
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
         List<Object[]> resposta = typedQuery.getResultList();
         assertFalse(resposta.isEmpty());
         resposta.forEach(item -> System.out.println(item[0] + " - " + item[1]));
+
     }
 
 }
