4614c654d4280dbc4de48c80d770208e5988bc56
Valdeir Nascimento <valdeircn11@gmail.com>
2023-01-25 22:43:26 -0300

    Externalizando queries em um arquivo XML

--- src/test/java/com/algaworks/ecommerce/jpql/NamedQueryTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/NamedQueryTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
 import com.algaworks.ecommerce.model.Produto;
 import org.junit.Test;
 
@@ -10,6 +11,21 @@ import java.util.List;
 import static org.junit.Assert.assertFalse;
 
 public class NamedQueryTest extends EntityManagerTest {
+
+
+    @Test
+    public void executarConsultaArquivoXMLEspecifico() {
+        TypedQuery<Pedido> typedQuery = entityManager.createNamedQuery("Pedido.todos", Pedido.class);
+        List<Pedido> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsultaArquivoXML() {
+        TypedQuery<Pedido> typedQuery = entityManager.createNamedQuery("Pedido.listar", Pedido.class);
+        List<Pedido> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void executarConsultar() {
