54fffee71ffd4af134e23f90a001315c65b561af
Valdeir Nascimento <valdeircn11@gmail.com>
2023-01-26 23:06:31 -0300

    Selecionando um atributo da entidade como retorno da consulta

--- src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
 import org.junit.Test;
 
@@ -9,9 +10,27 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-import static org.junit.Assert.assertNotNull;
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void selecionarUmAtributoParaRetorno() {
+        //select p.total from pedido p where p.id = 1
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<BigDecimal> criteriaQuery = criteriaBuilder.createQuery(BigDecimal.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root.get("total"));
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<BigDecimal> typedQuery = entityManager.createQuery(criteriaQuery);
+        BigDecimal total = typedQuery.getSingleResult();
+
+        assertEquals(new BigDecimal("2398.00"), total);
+    }
 
     @Test
     public void buscarPorId() {
