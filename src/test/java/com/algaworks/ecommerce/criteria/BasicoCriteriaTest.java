8b5faf00631f46af743e5d39e71bd9c12a7fe005
Valdeir Nascimento <valdeircn11@gmail.com>
2023-01-27 21:58:22 -0300

    Selecionando todos os produtos com criteria

--- src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
+++ src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
@@ -1,20 +1,34 @@
 package com.algaworks.ecommerce.criteria;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void retornarTodosOsProdutosExercicio() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
+        System.out.println(lista);
+        assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void selecionarUmAtributoParaRetorno() {
