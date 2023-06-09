d210802bc8a85754a7df94de787a25308f894097
Valdeir Nascimento <valdeircn11@gmail.com>
2023-06-09 20:34:34 -0300

    Usando funções para string

---
+++ src/test/java/com/algaworks/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -0,0 +1,51 @@
+package com.algaworks.ecommerce.criteria;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.Cliente_;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class FuncoesCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncaoString() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Cliente> root = criteriaQuery.from(Cliente.class);
+
+        criteriaQuery.multiselect(
+            root.get(Cliente_.nome),
+            criteriaBuilder.concat("Nome do cliente: ", root.get(Cliente_.nome)),
+            criteriaBuilder.length(root.get(Cliente_.nome)),
+            criteriaBuilder.locate(root.get(Cliente_.nome), "a"),
+            criteriaBuilder.substring(root.get(Cliente_.nome), 1, 2),
+            criteriaBuilder.lower(root.get(Cliente_.nome)),
+            criteriaBuilder.upper(root.get(Cliente_.nome)),
+            criteriaBuilder.trim(root.get(Cliente_.nome))
+        );
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+            arr[0] + "\n"
+                + ", concat: " + arr[1] + "\n"
+                + ", length: " + arr[2] + "\n"
+                + ", locate: " + arr[3] + "\n"
+                + ", substring: " + arr[4] + "\n"
+                + ", lower: " + arr[5] + "\n"
+                + ", upper: " + arr[6] + "\n"
+                + ", trim: " + arr[7] + "\n"
+        ));
+
+    }
+
+}
