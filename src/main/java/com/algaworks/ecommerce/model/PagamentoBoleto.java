9b7f11b9dc1e0b42021623b1fe8f3ff8b991ffc6
Valdeir Nascimento <valdeircn11@gmail.com>
2023-06-10 20:44:22 -0300

    Usando funções para datas

--- src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
+++ src/main/java/com/algaworks/ecommerce/model/PagamentoBoleto.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 
 @Getter
@@ -14,4 +15,8 @@ public class PagamentoBoleto extends Pagamento {
 
 	@Column(name = "codigo_barras", length = 100)
 	private String codigoBarras;
+
+	@Column(name = "data_vencimento")
+	private LocalDate dataVencimento;
+
 }
